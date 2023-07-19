bcbbd9ebae320055dd33bdcd91f595730a1aaec5
39takagikouta <kouta.takagi0728@gmail.com>
2023-07-19 18:57:37 +0900

    Fix:残りの要件を全て修正

--- app/controllers/events/attendances_controller.rb
+++ app/controllers/events/attendances_controller.rb
@@ -3,11 +3,15 @@
 class Events::AttendancesController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
-    event_attendance = current_user.attend(@event)
-    (@event.attendees - [current_user] + [@event.user]).uniq.each do |user|
-      NotificationFacade.attended_to_event(event_attendance, user)
+    if current_user.woman? || !@event.only_woman
+      event_attendance = current_user.attend(@event)
+      (@event.attendees - [current_user] + [@event.user]).uniq.each do |user|
+        NotificationFacade.attended_to_event(event_attendance, user)
+      end
+      redirect_back(fallback_location: root_path, success: '参加の申込をしました')
+    else
+      redirect_to root_path
     end
-    redirect_back(fallback_location: root_path, success: '参加の申込をしました')
   end
 
   def destroy
